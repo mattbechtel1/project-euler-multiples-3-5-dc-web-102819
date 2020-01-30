fbf0eb9cf20216c58a0f273ffc70f9a979e3f675
mattbechtel1 <bechma02@gmail.com>
2020-01-30 09:24:29 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+
+def multiplesOf3
+    (0...1000).select {|num| num % 3 == 0}
+end
+
+def multiplesOf5
+    (0...1000).select {|num| num % 5 == 0}
+end
+
+puts multiplesOf3.concat(multiplesOf5).uniq.reduce { |memo, num| memo + num}
+
+def collect_multiples(limit)
+    (1...limit).select { |num| num % 3 == 0 || num % 5 == 0}
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).reduce {|memo, num| memo + num}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1...self.limit).select { |num| num % 3 == 0 || num % 5 == 0}
+    end
+
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+
+end
